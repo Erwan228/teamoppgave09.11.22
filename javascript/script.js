94b3700b302b920bf8442a2fce23b062cd3a35fb
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2022-11-09 10:50:33 +0100

    meters og bilder

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,6 @@
 //model
 let site = document.getElementById(`app`);
-let mood; 
+let mood = '<img src="images/9140_Moogle.png">'; 
 let food = 0;
 let sleep = 0;
 let play = 0;
@@ -12,16 +12,26 @@ function updateView(){
     <h1>Dette er Mousse. Pass godt på Mousse!</h1>
     <div class="moogle-pics">${mood}</div>
     <div class="metersAndButtons">
-    <meter value="${food}" max="100"><button onclick="addValue()">Feed</button>
-    <meter value="${sleep}" max="100"><button onclick="addValue()">Sleep</button>
-    <meter value="${play}" max="100"><button onclick="addValue()">Play!</button>
+    <meter value="${food}" max="100"></meter><button onclick="addValue('food')">Feed</button>
+    <meter value="${sleep}" max="100"></meter><button onclick="addValue('sleep')">Sleep</button>
+    <meter value="${play}" max="100"></meter><button onclick="addValue('play')">Play!</button>
     </div>
     `;
 }
 
 //controller
 
-function addValue(){
-    
+function addValue(addPoints){
+        if (addPoints == 'food' && food < 100) food += 10;
+        if (addPoints == 'sleep' && sleep < 100) sleep += 10;
+        if (addPoints == 'play' && play < 100) play += 10;
+        updateView();
+}
+
+function decreasingValue(){
+    if (food == 70 || sleep == 70 || play == 70) mood = '<img src="images/AngryMoogle.jpg"';
+    if (food == 30 || sleep == 30 || play == 30) mood = '<img src="images/crying _moogle.png"';
+    if (food == 0 || sleep == 0 || play == 0) mood = '<img src=""';
+    updateView();
 }
 
